aad62b2012c7d36e2bf7bce937e25c9445c84e26
Mark Schmidt <mark.schmidt@hey.com>
2021-02-06 16:52:37 -0600

    Correct response?

--- server/function.py
+++ server/function.py
@@ -6,6 +6,10 @@ cache = {}
 REFRESH_WINDOW = 60  # seconds
 
 
+def success_response(resp):
+    return {"statusCode": 200, "body": resp}
+
+
 def lambda_handler(event, context):
     # First, hit the cache
     print(event)
@@ -13,12 +17,12 @@ def lambda_handler(event, context):
     if sport in cache:
         item, timestamp = cache[sport]
         if time.time() - timestamp < REFRESH_WINDOW:
-            return item
+            return success_response(item)
 
     if sport == "college-basketball":
         result = CollegeBasketball.getGames(False)
         cache[sport] = (result, time.time())
-        return result
+        return success_response(result)
 
     print(f"ERROR: unknown sport {sport}")
     return None
