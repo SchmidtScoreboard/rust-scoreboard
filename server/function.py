8268b10d69a021e196fc1fe3734b97ecc82a1caf
Mark Schmidt <mark.schmidt@hey.com>
2021-02-06 16:50:29 -0600

    Correctly use endpoint

--- server/function.py
+++ server/function.py
@@ -11,13 +11,13 @@ def lambda_handler(event, context):
     print(event)
     sport = event["path"][1:]
     if sport in cache:
-        item, timestamp = cache[event]
+        item, timestamp = cache[sport]
         if time.time() - timestamp < REFRESH_WINDOW:
             return item
 
     if sport == "college-basketball":
         result = CollegeBasketball.getGames(False)
-        cache[event] = (result, time.time())
+        cache[sport] = (result, time.time())
         return result
 
     print(f"ERROR: unknown sport {sport}")
