189d3adc7ef5c94830d6db7250067594b6e17726
Mark Schmidt <mark.schmidt@hey.com>
2024-04-22 16:19:41 -0500

    Fix hockey?

--- server/hockey.py
+++ server/hockey.py
@@ -81,7 +81,7 @@ class Hockey:
         period = data.get("period", 0)
         if period == 0 and "periodDescriptor" in data:
             period = data["periodDescriptor"].get("number", 0)
-            game["status"] = INVALID
+            game["status"] = "PREGAME" 
         if data.get("clock", None) is not None:
             clock = data["clock"]
             period_time = clock.get("timeRemaining", "20:00")
